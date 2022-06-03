e581301c1d453ee77f8d837cfda63176d6644186
yyyyyyounger <1049825685@qq.com>
2022-06-03 22:16:34 +0800

    新增UM map的WebView和index.js的備註

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ import App from "./App";
 import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);
+
+// VSCode運行
+// 要用CMD或者PowerShell運行
+// react-native run-android
--- view/Home/Map.js
+++ view/Home/Map.js
@@ -1,14 +1,21 @@
+// 使用WebView組件
+// https://juejin.cn/post/6978299338795532302
+
 import React, { Component } from "react";
 import { Text, View } from "react-native";
+import { WebView } from 'react-native-webview';
 
 export class Map extends Component{
     render() {
         return (
-            <View>
-                <Text>
-                    Map
-                </Text>
-            </View>
+            // <View>
+            //     <Text>
+            //         Map
+            //     </Text>
+            // </View>
+            <WebView 
+                source={{ uri:'https://maps.um.edu.mo/' }} 
+            />
         )
     }
 }
