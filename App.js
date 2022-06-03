b03db7c9fbc4e90c01c0d4b7fc0ee398b1928d19
boxmars <2803726925@qq.com>
2022-06-03 22:49:30 +0800

    Update App.js

--- App.js
+++ App.js
@@ -7,14 +7,7 @@
  */
 
 import React, { Component } from "react";
-import {
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    TouchableWithoutFeedback,
-    View,
-} from "react-native";
+import { ScrollView, StatusBar, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 import tw from "twrnc";
 import { SafeAreaProvider } from "react-native-safe-area-context/src/SafeAreaContext";
 import { NavigationContainer } from "@react-navigation/native";
@@ -40,24 +33,24 @@ class HomeFuncScreen extends Component {
     render() {
         let s = StyleSheet.create({
             borderColor: "#2F3A79",
-            borderWidth: 10,
-            borderRadius: 15,
+            borderWidth: 4,
+            borderRadius: 8,
             backgroundColor: "#fff",
             overlayColor: "#ffffff",
             marginBottom: 2,
         });
         return (
             <ScrollView style={tw.style("w-full", "h-full", "bg-white")}>
-                <View style={tw.style("flex", "flex-row", "my-5", "mx-5",'justify-around',"flex-wrap")}>
+                <View style={tw.style("flex", "flex-row", "mb-2", "mx-5", "justify-around", "flex-wrap",'mt-4')}>
                     <View>
-                        <TouchableWithoutFeedback   onPress={this.goToMapScreen}>
-                            <View style={s} >
+                        <TouchableWithoutFeedback onPress={this.goToMapScreen}>
+                            <View style={s}>
                                 <Image
                                     source={require("./static/img/Map.png")}
                                     style={tw.style("w-17", "h-17")} imageStyle={{ borderRadius: 50 }} />
                             </View>
-                        </TouchableWithoutFeedback   >
-                        <Text style={tw.style("text-center", "text-black", "text-sm",'mb-2')}>Map</Text>
+                        </TouchableWithoutFeedback>
+                        <Text style={tw.style("text-center", "text-black", "text-sm", "mb-1")}>Map</Text>
                     </View>
 
                     <View>
@@ -66,27 +59,36 @@ class HomeFuncScreen extends Component {
                                 source={require("./static/img/UMNews.png")}
                                 style={tw.style("w-17", "h-17")} imageStyle={{ borderRadius: 50 }} />
                         </View>
-                        <Text style={tw.style("text-center", "text-black", "text-sm",'mb-2')}>UM News</Text>
+                        <Text style={tw.style("text-center", "text-black", "text-sm", "mb-1")}>UM News</Text>
                     </View>
-
                     <View>
                         <View style={s}>
                             <Image
                                 source={require("./static/img/Scholarship.png")}
                                 style={tw.style("w-17", "h-17")} imageStyle={{ borderRadius: 50 }} />
                         </View>
-                        <Text style={tw.style("text-center", "text-black", "text-sm",'mb-2')}>Whole Person</Text>
+                        <Text style={tw.style("text-center", "text-black", "text-xs", "mb-1")}>Whole Person</Text>
                     </View>
+
+                    {/*<View>*/}
+                    {/*    <View style={s}>*/}
+                    {/*        <Image*/}
+                    {/*            source={require("./static/img/Scholarship.png")}*/}
+                    {/*            style={tw.style("w-17", "h-17")} imageStyle={{ borderRadius: 50 }} />*/}
+                    {/*    </View>*/}
+                    {/*    <Text style={tw.style("text-center", "text-black", "text-sm", "mb-1")}>Whole Person</Text>*/}
+                    {/*</View>*/}
+
                 </View>
 
-                <View style={tw.style("flex", "flex-row", "my-5", "mx-5",'justify-around',"flex-wrap")}>
+                <View style={tw.style("flex", "flex-row", "my-2", "mx-5", "justify-around", "flex-wrap")}>
                     <View>
                         <View style={s}>
                             <Image
                                 source={require("./static/img/Calendar.png")}
                                 style={tw.style("w-17", "h-17")} imageStyle={{ borderRadius: 50 }} />
                         </View>
-                        <Text style={tw.style("text-center", "text-black", "text-sm",'mb-2')}>Calendar</Text>
+                        <Text style={tw.style("text-center", "text-black", "text-sm", "mb-2")}>Calendar</Text>
                     </View>
 
                     <View>
@@ -108,21 +110,21 @@ class HomeFuncScreen extends Component {
                     </View>
                 </View>
 
-                <View style={tw.style("flex", "flex-row", "my-5", "mx-5",'justify-around',"flex-wrap")}>
+                <View style={tw.style("flex", "flex-row", "my-2", "mx-5", "justify-around", "flex-wrap")}>
                     <View>
                         <View style={s}>
                             <Image
                                 source={require("./static/img/What2Reg.png")}
                                 style={tw.style("w-17", "h-17")} imageStyle={{ borderRadius: 50 }} />
                         </View>
-                        <Text style={tw.style("text-center", "text-black", "text-sm",'mb-2')}>What2Reg</Text>
+                        <Text style={tw.style("text-center", "text-black", "text-sm", "mb-2")}>What2Reg</Text>
                     </View>
 
                     <View>
                         <View style={{
                             borderColor: "#fff",
-                            borderWidth: 10,
-                            borderRadius: 15,
+                            borderWidth: 4,
+                            borderRadius: 8,
                             backgroundColor: "#fff",
                             overlayColor: "#ffffff",
                             marginBottom:5
@@ -135,8 +137,8 @@ class HomeFuncScreen extends Component {
                     <View>
                         <View style={{
                             borderColor: "#fff",
-                            borderWidth: 10,
-                            borderRadius: 15,
+                            borderWidth: 4,
+                            borderRadius: 8,
                             backgroundColor: "#fff",
                             overlayColor: "#ffffff",
                             marginBottom:5
@@ -146,8 +148,15 @@ class HomeFuncScreen extends Component {
                         </View>
                     </View>
                 </View>
-
-
+            <View style={tw.style('rounded-full','w-60','mx-auto','pt-40')}>
+                <View style={{
+                    backgroundColor: "#2F3A79",
+                    borderRadius: 100,
+                    paddingVertical:3
+                }}>
+                    <Text style={tw.style('text-white','text-center','text-xl')}>Login by UM ID 😍</Text>
+                </View>
+            </View>
             </ScrollView>
         );
     }
@@ -157,17 +166,27 @@ const HomeStack = createNativeStackNavigator();
 
 function HomeScreen() {
     return (
-        <HomeStack.Navigator screenOptions={{
+        <HomeStack.Navigator >
+            <HomeStack.Screen name="HomeFunc" component={HomeFuncScreen} options={
+                {
+                    title:'UM ALL',
+                    headerStyle:{
+                        backgroundColor:'#2F3A79'
+                    },
+                    headerTintColor:'#fff'
+                }
+            }/>
+            <HomeStack.Screen name="Map" component={Map} options={{
                 headerShown:false
             }
-        }>
-            <HomeStack.Screen name="HomeFunc" component={HomeFuncScreen} />
-            <HomeStack.Screen name="Map" component={Map} />
+            }/>
         </HomeStack.Navigator>
     );
 
 }
-
+function getBusData(){
+    return 
+}
 class BusScreen extends Component {
     render() {
         return (
@@ -180,12 +199,12 @@ class BusScreen extends Component {
     }
 }
 
-class BBSScreen extends Component {
+class NewsScreen extends Component {
     render() {
         return (
             <View>
                 <Text>
-                    BBS
+                    News
                 </Text>
             </View>
         );
@@ -223,12 +242,12 @@ function App() {
                                     iconName = focused
                                         ? 'home'
                                         : 'home-outline';
-                                } else if (route.name === 'Bus') {
-                                    iconName = focused ? 'bus' : 'bus-outline';
-                                } else if (route.name === 'BBS') {
-                                    iconName = focused ? 'boat' : 'boat-outline';
-                                } else if (route.name === 'Me') {
-                                    iconName = focused ? 'person' : 'person-outline';
+                                } else if (route.name === "Bus") {
+                                    iconName = focused ? "bus" : "bus-outline";
+                                } else if (route.name === "News") {
+                                    iconName = focused ? "boat" : "boat-outline";
+                                } else if (route.name === "Me") {
+                                    iconName = focused ? "person" : "person-outline";
                                 }
 
                                 // You can return any component that you like here!
@@ -263,22 +282,22 @@ function App() {
                     }}/>
                     <Tab.Screen name="Bus" component={BusScreen} options={{
                         headerStyle: {
-                            backgroundColor: '#2F3A79',
+                            backgroundColor: "#2F3A79",
                         },
-                        headerTintColor: '#fff',
+                        headerTintColor: "#fff",
                         headerTitleStyle: {
-                            fontWeight: 'bold',
+                            fontWeight: "bold",
                         },
-                    }}/>
-                    <Tab.Screen name="BBS" component={BBSScreen} options={{
+                    }} />
+                    <Tab.Screen name="News" component={NewsScreen} options={{
                         headerStyle: {
-                            backgroundColor: '#2F3A79',
+                            backgroundColor: "#2F3A79",
                         },
-                        headerTintColor: '#fff',
+                        headerTintColor: "#fff",
                         headerTitleStyle: {
-                            fontWeight: 'bold',
+                            fontWeight: "bold",
                         },
-                    }}/>
+                    }} />
                     <Tab.Screen name="Me" component={UserScreen} options={{
                         headerStyle: {
                             backgroundColor: '#2F3A79',
