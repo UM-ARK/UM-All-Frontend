320fcd40fdae3d337705fc3285fd4cbee4234af4
yyyyyyounger <1049825685@qq.com>
2022-06-21 15:58:28 +0800

    :art: 保證gesture-handler庫正常使用

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-/**
- * @format
- */
-
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
-// import App from './src/test/test';
 import {name as appName} from './app.json';
 
 // 關閉模擬器內的黃色警告
