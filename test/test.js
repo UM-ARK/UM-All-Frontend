da2d2c1e6d6dc2b9f9ab8eacd433b420f9996f86
yyyyyyounger <1049825685@qq.com>
2022-06-22 22:27:08 +0800

    :lipstick: 新增主頁設計稿佈局

--- test/test.js
+++ test/test.js
@@ -9,13 +9,13 @@ import {
 } from 'react-native';
 
 import {COLOR_DIY} from '../src/utils/uiMap';
+import { pxToDp } from '../src/utils/stylesKits';
 
 // 文檔：https://github.com/dohooo/react-native-reanimated-carousel/
 import Carousel from 'react-native-reanimated-carousel';
 import {Header} from 'react-native-elements'; // 4.0 Beta版
 import {NavigationContext} from '@react-navigation/native'
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { pxToDp } from '../src/utils/stylesKits';
 
 const {width: PAGE_WIDTH} = Dimensions.get('window');
 
@@ -23,12 +23,6 @@ class TestScreen extends Component {
     static contextType = NavigationContext;
     // this.context === this.props.navigation
     render() {
-        const baseOptions = {
-            vertical: false,
-            width: PAGE_WIDTH,
-            height: PAGE_WIDTH / 2,
-        };
-
         return (
             <View style={{flex: 1, backgroundColor: COLOR_DIY.bg_color}}>
                 <Header
@@ -51,31 +45,6 @@ class TestScreen extends Component {
                     }}
                     statusBarProps={{backgroundColor:COLOR_DIY.bg_color, barStyle:'dark-content'}}
                 />
-
-                <View style={{paddingTop: 50}}>
-                    <Carousel
-                        {...baseOptions}
-                        loop
-                        autoPlay={true}
-                        autoPlayInterval={2000}
-                        data={[1, 2, 3]}
-                        renderItem={({index}: {index: number}) => (
-                            <View
-                                style={{
-                                    borderWidth: 1,
-                                    borderColor: 'red',
-                                    flex: 1,
-                                    borderRadius: 20,
-                                    flexDirection: 'row',
-                                    justifyContent: 'space-between',
-                                    alignItems: 'center',
-                                    paddingHorizontal: 10,
-                                }}>
-                                <Text>{index}</Text>
-                            </View>
-                        )}
-                    />
-                </View>
             </View>
         );
     }
