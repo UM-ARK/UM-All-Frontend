822f12e520d35cc532aa16da6c4208066eebe03d
yyyyyyounger <1049825685@qq.com>
2022-06-25 16:26:55 +0800

    :art: 新增me頁setting頁跳轉

---
+++ src/pages/TabbarPages/me/pages/AppSetting.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {
+    ScrollView,
+    View,
+    Text,
+    SafeAreaView,
+    Dimensions,
+    StyleSheet,
+    TouchableOpacity,
+} from 'react-native';
+
+class Setting extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <View>
+                <Text>App設置頁</Text>
+            </View>
+        );
+    }
+}
+
+export default Setting;
