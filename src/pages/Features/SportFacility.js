281d285e0196fba0e85f75dfae5da06648c8f305
yyyyyyounger <1049825685@qq.com>
2022-06-27 22:44:59 +0800

    :art: 新增體育設施頁、組織詳情頁的路由

---
+++ src/pages/Features/SportFacility.js
@@ -0,0 +1,45 @@
+// 體育設施頁
+import React, { Component } from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+
+import {COLOR_DIY} from '../../utils/uiMap'
+import {pxToDp} from '../../utils/stylesKits'
+
+import { WebView } from 'react-native-webview';
+import {Header} from 'react-native-elements'; // 4.0 Beta版
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+class SportFacility extends Component{
+    render() {
+        return (
+            <View style={{flex:1}}>
+                <Header
+                    backgroundColor={COLOR_DIY.bg_color}
+                    leftComponent={
+                        <TouchableOpacity onPress={()=>this.props.navigation.goBack()}>
+                            <Ionicons
+                                name="chevron-back-outline"
+                                size={pxToDp(25)}
+                                color={COLOR_DIY.black.main}
+                            />
+                        </TouchableOpacity>
+                    }
+                    centerComponent={{
+                        text: 'UM 體育設施',
+                        style: {
+                            color: COLOR_DIY.black.main,
+                            fontSize: pxToDp(15),
+                        },
+                    }}
+                    statusBarProps={{backgroundColor:'transparent', barStyle:'dark-content'}}
+                />
+
+                <WebView 
+                    source={{ uri:'https://facility.osa.um.edu.mo/sports-facilities/?lang=zh-hant' }} 
+                />
+            </View>
+        )
+    }
+}
+
+export default SportFacility;
