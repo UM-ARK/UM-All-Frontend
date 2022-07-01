6c5886e5c431cf3ca8877ecbf3b6fcaeba33c84d
yyyyyyounger <1049825685@qq.com>
2022-07-01 22:09:58 +0800

    :art: 改善縮進

--- src/pages/TabbarPages/news/components/ClubCard.js
+++ src/pages/TabbarPages/news/components/ClubCard.js
@@ -1,65 +1,106 @@
 import React, {Component} from 'react';
-import {View, Image, Text, ImageBackground, Dimensions, TouchableOpacity} from 'react-native';
+import {
+    View,
+    Image,
+    Text,
+    ImageBackground,
+    Dimensions,
+    TouchableOpacity,
+} from 'react-native';
 
-import {COLOR_DIY} from '../../../../utils/uiMap'
-import {pxToDp} from '../../../../utils/stylesKits'
+import {COLOR_DIY} from '../../../../utils/uiMap';
+import {pxToDp} from '../../../../utils/stylesKits';
 
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import {NavigationContext} from '@react-navigation/native'
-import FastImage from 'react-native-fast-image'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {NavigationContext} from '@react-navigation/native';
+import FastImage from 'react-native-fast-image';
 
 class EventCard extends Component {
     // NavigationContext組件可以在非基頁面拿到路由信息
     // this.context === this.props.navigation 等同效果
     static contextType = NavigationContext;
 
-	state = {
-        data : this.props.data,
-    }
+    state = {
+        data: this.props.data,
+    };
 
-	render() {
-        // 解構this.state數據
-        const {data} = this.state;
-        // 解構dataList數據
-        const {imgUrl, name, tag} = data;
+    // 處理點擊跳轉邏輯
+    handleJumpToDetail = () => {
+        const {name} = this.state.data;
+        const {index} = this.props;
+        this.context.navigate('ClubDetail', {
+            name,
+            index,
+        });
+    };
+
+    render() {
+        // 解構this.state.dataList數據
+        const {imgUrl, name, tag} = this.state.data;
         // 當前點擊的數組下標，對應響應的組織
         const {index} = this.props;
         // 解構全局ui設計顏色
         const {white, black, viewShadow, themeColor} = COLOR_DIY;
         return (
-			<TouchableOpacity style={{...this.props.style}} activeOpacity={0.8} 
-                onPress={()=>{
-                    // alert(`進入下標為 ${index} 組織詳情頁`)
-                    this.context.navigate('ClubDetail', {
-                        name,
-                        index,
-                    })
-                }}
-                disabled={this.props.touchDisable}
-            >
-                <View style={{
-                    backgroundColor:white, borderRadius:pxToDp(8),
-                    justifyContent:'space-around', alignItems:'center', 
-                    marginTop:pxToDp(2),
-                    padding:pxToDp(10), paddingHorizontal:pxToDp(4), 
-                    ...viewShadow
-                }}>
+            <View style={{...this.props.style}}>
+                <View
+                    style={{
+                        backgroundColor: white,
+                        borderRadius: pxToDp(8),
+                        justifyContent: 'space-around',
+                        alignItems: 'center',
+                        marginTop: pxToDp(2),
+                        padding: pxToDp(10),
+                        paddingHorizontal: pxToDp(4),
+                        ...viewShadow,
+                    }}>
                     {/* 社團 / 組織 Logo */}
-                    <View>
-                        <FastImage source={{uri:imgUrl}} style={{width:pxToDp(70),height:pxToDp(70), borderRadius:50}} resizeMode={FastImage.resizeMode.contain} />
-                    </View>
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        onPress={this.handleJumpToDetail}
+                        disabled={this.props.touchDisable}>
+                        <FastImage
+                            source={{uri: imgUrl}}
+                            style={{
+                                width: pxToDp(70),
+                                height: pxToDp(70),
+                                borderRadius: 50,
+                            }}
+                            resizeMode={FastImage.resizeMode.contain}
+                        />
+                    </TouchableOpacity>
 
                     {/* 組織名 */}
-                    <View style={{flexDirection:'row', justifyContent:'center', alignItems:'center', marginTop:pxToDp(5)}}>
-                        <Text style={{color:black.main, fontSize:pxToDp(12)}} numberOfLines={1}>{name}</Text>
-                    </View>
+                    <TouchableOpacity
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            marginTop: pxToDp(5),
+                        }}
+                        activeOpacity={0.8}
+                        onPress={this.handleJumpToDetail}
+                        disabled={this.props.touchDisable}>
+                        <Text
+                            style={{color: black.main, fontSize: pxToDp(12)}}
+                            numberOfLines={1}>
+                            {name}
+                        </Text>
+                    </TouchableOpacity>
 
                     {/* 組織標籤 */}
-                    <Text style={{color:themeColor, fontSize:pxToDp(10), marginTop:pxToDp(5)}}>#{tag}</Text>
+                    <Text
+                        style={{
+                            color: themeColor,
+                            fontSize: pxToDp(10),
+                            marginTop: pxToDp(5),
+                        }}>
+                        #{tag}
+                    </Text>
                 </View>
-			</TouchableOpacity>
-		);
-	}
+            </View>
+        );
+    }
 }
 
 export default EventCard;
