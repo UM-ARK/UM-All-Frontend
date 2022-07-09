f2546c51239fed401a8f97a09b8264a888235810
ImK4Lok <samkalok107@gmail.com>
2022-07-09 18:56:37 +0200

    修復BlurView的不對稱問題

--- src/components/BlurViewWrapper.js
+++ src/components/BlurViewWrapper.js
@@ -7,7 +7,7 @@ const BlurViewWrapper = (props) => {
             <ImageBackground
                 source={{uri: props.url}}
                 blurRadius={Platform.OS === 'ios' ? props.blurRadius + 5 : props.blurRadius}
-                style={[styles.blurImageStyle, {height: props.blurHeight, width: props.width}]}
+                style={[styles.blurImageStyle, {height: props.height, width: props.width}]}
             >
                 <View style={{backgroundColor: props.bgColor, height: props.blurHeight}}>
                     {props.children}
@@ -20,7 +20,7 @@ const BlurViewWrapper = (props) => {
 const styles = StyleSheet.create({
     blurWrap: {
         // height: 150,
-        // overflow: 'hidden',
+        overflow: 'hidden',
         width: '100%',
         position: 'absolute',
         bottom: 0,
