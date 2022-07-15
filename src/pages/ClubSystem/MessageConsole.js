a8199bf1d99f180843fefa1493b16dba0d8db621
yyyyyyounger <1049825685@qq.com>
2022-07-15 18:28:52 +0800

    :art: 刪除舊數據類型

--- src/pages/ClubSystem/MessageConsole.js
+++ src/pages/ClubSystem/MessageConsole.js
@@ -10,69 +10,6 @@ import {Header, SpeedDial} from '@rneui/themed';
 
 const {bg_color, themeColor, white, viewShadow} = COLOR_DIY;
 
-// 模擬服務器請求回來的data
-const messages = [
-    {
-        user: {
-            _id: '0',
-            name: '澳大電子通告',
-            avatar: 'http://images.jjl.cn/ugc/2018/1129/20181129152330319.png',
-        },
-        message_history: [
-            {
-                content:
-                    '請全澳居民於6月22日內進行一次快速抗原檢測。知道了嗎阿是離開打飛機拉克絲定積分了卡時代峻峰',
-                time: '2022/6/26 2:30',
-                unread: 1,
-            },
-            {
-                content: 'hahahaha',
-                time: '2022/6/26 1:30',
-                unread: 1,
-            },
-        ],
-    },
-    {
-        user: {
-            _id: '3',
-            name: '鄭裕彤書院(CYTC)',
-            avatar: 'https://cytc.rc.um.edu.mo/wp-content/uploads/2019/09/CYTC-Logo-2014-Purple-1.png',
-        },
-        message_history: [
-            {
-                content:
-                    '書院活動 purple dating 現已開啟，一切唉算了東風科技唉算了到科技付。',
-                time: '2022/6/26 2:30',
-                unread: 0,
-            },
-            {
-                content: 'hahahaha',
-                time: '2022/6/26 1:30',
-                unread: 0,
-            },
-        ],
-    },
-    {
-        user: {
-            _id: '4',
-            name: '電腦學會',
-            avatar: 'https://info.umsu.org.mo/storage/affiliate/images/da6f2ec4b5ec3216f9344453f796a97c.jpg',
-        },
-        message_history: [
-            {
-                content: '遊戲工作坊',
-                time: '2022/6/26 2:30',
-                unread: 1,
-            },
-            {
-                content: 'hahahaha',
-                time: '2022/6/26 1:30',
-                unread: 1,
-            },
-        ],
-    },
-];
-
 class MessageConsole extends Component {
     state = {
         // 打開右下角新建消息類型按鈕
