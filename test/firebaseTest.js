cbc7b62b8133a41baeacd76e0df3bbc028bb12b9
yyyyyyounger <1049825685@qq.com>
2022-07-17 17:02:37 +0800

    :sparkles: 推送設置

---
+++ test/firebaseTest.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {View, Text, Button} from 'react-native';
+
+import messaging from '@react-native-firebase/messaging';
+import {pxToDp} from '../src/utils/stylesKits';
+
+class Test extends Component {
+    async checkToken() {
+        const fcmToken = await messaging().getToken();
+        if (fcmToken) {
+            console.log(fcmToken);
+            alert(fcmToken);
+        }
+    }
+
+    render() {
+        console.log(messaging);
+        return (
+            <View style={{style: 1, paddingTop: pxToDp(50)}}>
+                <Text>Test</Text>
+                <Button title="自動初始化" onPress={this.checkToken}></Button>
+            </View>
+        );
+    }
+}
+
+export default Test;
