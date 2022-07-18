5df7d229464ea0c79eead297e73c349c5f0dd5e0
yyyyyyounger <1049825685@qq.com>
2022-07-18 13:57:50 +0800

    :art: 更新推送測試頁

--- test/firebaseTest.js
+++ test/firebaseTest.js
@@ -2,14 +2,21 @@ import React, {Component} from 'react';
 import {View, Text, Button} from 'react-native';
 
 import messaging from '@react-native-firebase/messaging';
+
 import {pxToDp} from '../src/utils/stylesKits';
 
 class Test extends Component {
     async checkToken() {
         const fcmToken = await messaging()
             .getToken()
-            .then(res => console.log('res', res))
-            .catch(err => console.error('err', err));
+            .then(res => {
+                console.log('res', res);
+                alert(res);
+            })
+            .catch(err => {
+                console.error('err', err);
+                alert(err);
+            });
         if (fcmToken) {
             console.log(fcmToken);
             alert(fcmToken);
