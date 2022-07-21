9572f2a12f96f7702c6526430e20ef74970b7196
ImK4Lok <samkalok107@gmail.com>
2022-07-21 21:58:31 +0100

    add hyperlink long press action
    long press for copying text into clipboard and display a toast notification as well

--- src/components/HyperlinkText.js
+++ src/components/HyperlinkText.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import {Text} from 'react-native';
 import Hyperlink from 'react-native-hyperlink';
+import Clipboard from '@react-native-clipboard/clipboard';
+import Toast from 'react-native-toast-message';
 
-const HyperlinkText = ({children, linkStyle, style }) => {
+const HyperlinkText = ({children, linkStyle, style}) => {
+    const handleLink = (url, text) => {};
+
+    const copyToClipboard = (url, text) => {
+        console.log('copying');
+        Clipboard.setString(text);
+        showClipedMessage();
+        console.log('copied');
+    };
+
+    const showClipedMessage = () => {
+        Toast.show({
+            type: 'info',
+            text1: `Text copied.`,
+        });
+    };
+
     return (
-        <Hyperlink
-            linkStyle={linkStyle}
-            onPress={link => console.log(link)}>
-            <Text style={style}>{children}</Text>
-        </Hyperlink>
+        <>
+            <Hyperlink
+                linkStyle={linkStyle}
+                // linkDefault={true}
+                onPress={(url, text) => handleLink(url, text)}
+                onLongPress={(url, text) => {
+                    copyToClipboard(url, text);
+                }}>
+                <Text style={style}>{children}</Text>
+            </Hyperlink>
+            <Toast 
+                position="top"
+                topOffset={0}
+                visibilityTime={1000} 
+                />
+        </>
     );
 };
 
