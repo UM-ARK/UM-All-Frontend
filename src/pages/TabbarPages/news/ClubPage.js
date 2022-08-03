33c3432be95980f894cf9b82fbe280df154ee52a
yyyyyyounger <1049825685@qq.com>
2022-08-03 22:14:03 +0800

    :sparkles: 新增組織頁的篩選功能

--- src/pages/TabbarPages/news/ClubPage.js
+++ src/pages/TabbarPages/news/ClubPage.js
@@ -1,21 +1,30 @@
 import React, {Component} from 'react';
-import {Text, View, Dimensions, RefreshControl} from 'react-native';
+import {
+    Text,
+    View,
+    Dimensions,
+    RefreshControl,
+    TouchableOpacity,
+} from 'react-native';
 
 import {COLOR_DIY} from '../../../utils/uiMap';
 import {pxToDp} from '../../../utils/stylesKits';
 import {BASE_URI, BASE_HOST, GET} from '../../../utils/pathMap';
+import {clubTagList, clubTagMap} from '../../../utils/clubMap';
 import Loading from '../../../components/Loading';
-
 import ClubCard from './components/ClubCard';
 
 import {FlatGrid} from 'react-native-super-grid';
 import axios from 'axios';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import ActionSheet from 'react-native-actionsheet';
 
-const {themeColor} = COLOR_DIY;
+const {themeColor, black, white} = COLOR_DIY;
 
 const {width: PAGE_WIDTH} = Dimensions.get('window');
 const COMPONENT_WIDTH = PAGE_WIDTH * 0.25;
 
+originClubDataList = [];
 class ClubPage extends Component {
     constructor() {
         super();
@@ -39,6 +48,7 @@ class ClubPage extends Component {
                     clubDataList.map(itm => {
                         itm.logo_url = BASE_HOST + itm.logo_url;
                     });
+                    originClubDataList = clubDataList;
                     this.setState({clubDataList, isLoading: false});
                 } else {
                     alert('Warning:', message);
@@ -49,41 +59,118 @@ class ClubPage extends Component {
             });
     }
 
+    renderClub = () => {
+        const {clubDataList} = this.state;
+        return (
+            <FlatGrid
+                // 每个项目的最小宽度或高度（像素）
+                itemDimension={COMPONENT_WIDTH}
+                data={clubDataList}
+                // 每個項目的間距
+                spacing={pxToDp(12)}
+                renderItem={({item}) => {
+                    return <ClubCard data={item} />;
+                }}
+                // 所有項目末尾渲染，防Tabbar遮擋
+                ListFooterComponent={() => (
+                    <View>
+                        <Text
+                            style={{
+                                color: black.third,
+                                alignSelf: 'center',
+                                fontSize: pxToDp(12),
+                            }}>
+                            下拉可刷新頁面~
+                        </Text>
+                        <View style={{marginTop: pxToDp(50)}}></View>
+                    </View>
+                )}
+                refreshControl={
+                    <RefreshControl
+                        colors={[themeColor]}
+                        tintColor={themeColor}
+                        refreshing={this.state.isLoading}
+                        onRefresh={() => {
+                            // 展示Loading標識
+                            this.setState({isLoading: true});
+                            this.getData();
+                        }}
+                    />
+                }
+                keyExtractor={item => item._id}
+            />
+        );
+    };
+
+    clubFilter = tag => {
+        this.setState({isLoading: true});
+        // const {clubDataList} = this.state;
+
+        var filter = [tag];
+        var result = originClubDataList.filter(a => {
+            return filter.some(f => f === a.tag);
+        });
+        this.setState({clubDataList: result, isLoading: false});
+    };
+
+    renderFilter = () => {
+        let optionsList = [];
+        clubTagList.map(itm => {
+            optionsList.push(clubTagMap(itm));
+        });
+        optionsList.push('默認');
+        optionsList.push('取消');
+        return (
+            <View>
+                <TouchableOpacity
+                    onPress={() => this.ActionSheet.show()}
+                    activeOpacity={0.8}
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        alignSelf: 'center',
+                        marginTop: pxToDp(8),
+                    }}>
+                    <Text style={{color: black.third}}>篩選</Text>
+                    <Ionicons
+                        name={
+                            this.state.applyFilter
+                                ? 'md-funnel'
+                                : 'md-funnel-outline'
+                        }
+                        size={pxToDp(10)}
+                        color={black.third}
+                        style={{marginLeft: pxToDp(5)}}
+                    />
+                </TouchableOpacity>
+
+                {/* 選擇彈窗 */}
+                <ActionSheet
+                    ref={o => (this.ActionSheet = o)}
+                    options={optionsList}
+                    cancelButtonIndex={optionsList.length - 1}
+                    destructiveButtonIndex={optionsList.length - 1}
+                    onPress={index => {
+                        if (clubTagList[index]) {
+                            this.clubFilter(clubTagList[index]);
+                        } else if (index == optionsList.length - 2) {
+                            this.setState({clubDataList: originClubDataList});
+                        }
+                    }}
+                />
+            </View>
+        );
+    };
+
     render() {
         const {clubDataList, isLoading} = this.state;
         return (
             <View style={{flex: 1, backgroundColor: COLOR_DIY.bg_color}}>
                 {clubDataList != undefined && !isLoading ? (
-                    // TODO: 排序、篩選
-                    <FlatGrid
-                        style={{flex: 1}}
-                        // 每个项目的最小宽度或高度（像素）
-                        itemDimension={COMPONENT_WIDTH}
-                        data={clubDataList}
-                        // 每個項目的間距
-                        spacing={pxToDp(12)}
-                        renderItem={({item}) => (
-                            <View style={{flex: 1}}>
-                                <ClubCard data={item}></ClubCard>
-                            </View>
-                        )}
-                        // 所有項目末尾渲染，防Tabbar遮擋
-                        ListFooterComponent={() => (
-                            <View style={{marginTop: pxToDp(50)}}></View>
-                        )}
-                        refreshControl={
-                            <RefreshControl
-                                colors={[themeColor]}
-                                tintColor={themeColor}
-                                refreshing={this.state.isLoading}
-                                onRefresh={() => {
-                                    // 展示Loading標識
-                                    this.setState({isLoading: true});
-                                    this.getData();
-                                }}
-                            />
-                        }
-                    />
+                    <View style={{flex: 1}}>
+                        {this.renderFilter()}
+                        {this.renderClub()}
+                    </View>
                 ) : (
                     <View
                         style={{
