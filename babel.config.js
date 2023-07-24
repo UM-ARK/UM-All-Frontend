148eb8bdf25c07b7dd135878be65775e8a26e6a7
yyyyyyounger <55580370+yyyyyyounger@users.noreply.github.com>
2023-07-24 16:02:52 +0800

    Update babel.config.js
    Reanimated plugin has to be listed last.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    'react-native-reanimated/plugin',
-    ['@babel/plugin-proposal-decorators', {'legacy':true}]
+    ['@babel/plugin-proposal-decorators', {'legacy':true}],
+    'react-native-reanimated/plugin'
   ],
 };
