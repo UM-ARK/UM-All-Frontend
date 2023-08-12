b4555a962dd89ea3098f90f28f85762e55f6cb00
yyyyyyounger <1049825685@qq.com>
2023-08-12 11:46:25 +0800

    :sparkles: 新增Header組件返回按鈕的震動

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 // 普通的只帶返回按鈕的白底黑字Header，需傳遞title屬性
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
     ScrollView,
     View,
@@ -10,13 +10,14 @@ import {
     TouchableOpacity,
 } from 'react-native';
 
-import {COLOR_DIY} from '../utils/uiMap';
+import { COLOR_DIY } from '../utils/uiMap';
 import { scale } from 'react-native-size-matters';
 
 // 第三方庫
-import {Header} from '@rneui/themed';
+import { Header } from '@rneui/themed';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import {NavigationContext} from '@react-navigation/native';
+import { NavigationContext } from '@react-navigation/native';
+import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
 
 class HeaderDIY extends Component {
     // NavigationContext組件可以在非基頁面拿到路由信息
@@ -28,7 +29,10 @@ class HeaderDIY extends Component {
             <Header
                 backgroundColor={COLOR_DIY.bg_color}
                 leftComponent={
-                    <TouchableOpacity onPress={() => this.context.goBack()}>
+                    <TouchableOpacity onPress={() => {
+                        ReactNativeHapticFeedback.trigger('soft');
+                        this.context.goBack();
+                    }}>
                         <Ionicons
                             name="chevron-back-outline"
                             size={scale(25)}
