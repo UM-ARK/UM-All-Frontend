0d6e918ac9f3a63c54dc092a30774ff464938ec2
Rookie, LIN <1049825685@qq.com>
2024-12-15 18:28:38 +0800

    :bug: 解除一些console的warning

--- src/pages/TabbarPages/info/home/index.js
+++ src/pages/TabbarPages/info/home/index.js
@@ -606,7 +606,6 @@ class HomeScreen extends Component {
                     zIndex: 999,
                     position: 'absolute',
                 }}
-                ref="headInstance"
                 // 設定所有可吸附的屏幕位置 0,0為屏幕中心
                 snapPoints={[
                     { x: -scale(140), y: -verticalScale(220) },
--- src/pages/TabbarPages/what2Reg/pages/Comment.js
+++ src/pages/TabbarPages/what2Reg/pages/Comment.js
@@ -262,7 +262,6 @@ export default class Comment extends Component {
                     zIndex: 999,
                     position: 'absolute',
                 }}
-                ref="headInstance"
                 // 設定所有可吸附的屏幕位置 0,0為屏幕中心
                 snapPoints={[
                     { x: -scale(140), y: -verticalScale(220) },
