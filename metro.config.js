e208171e31e3948702b98da9d0fda049143ce8d9
Rookie, LIN <1049825685@qq.com>
2025-08-23 02:31:18 +0800

    :arrow_up: 嘗試升級reanimated和handler的依賴問題
    白屏尚未解決

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
 
 /**
  * Metro configuration
@@ -8,4 +9,8 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// First merge the default config with any custom config
+const mergedConfig = mergeConfig(getDefaultConfig(__dirname), config);
+
+// Then wrap the merged config with Reanimated's metro config so both take effect
+module.exports = wrapWithReanimatedMetroConfig(mergedConfig);
