daea4bb5dfb1cf9ca9beed439ad31407376465ff
Rookie, LIN <1049825685@qq.com>
2026-01-05 17:12:38 +0800

    :art: 整理expo android build

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+import { registerRootComponent } from 'expo';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
-import { AppRegistry, LogBox } from 'react-native';
+import { LogBox } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
 import './src/i18n/i18n';
+// import { scale, verticalScale } from 'react-native-size-matters';
+
+// 覆蓋原有的 scale 和 verticalScale，使其返回整數
+// const originalScale = scale;
+// const originalVerticalScale = verticalScale;
+// global.scale = (size) => Math.round(originalScale(size));
+// global.verticalScale = (size) => Math.round(originalVerticalScale(size));
 
 // 關閉模擬器內的黃色警告
-LogBox.ignoreLogs(['Warning: ...']);    // Ignore log notification by message
-LogBox.ignoreAllLogs();                 // Ignore all log notifications
+LogBox.ignoreLogs(['Warning: ...']);
+LogBox.ignoreAllLogs();
 
-AppRegistry.registerComponent(appName, () => () => (
+// 創建包裝組件
+const RootApp = () => (
     <GestureHandlerRootView>
         <App />
     </GestureHandlerRootView>
-));
+);
+
+registerRootComponent(RootApp);
