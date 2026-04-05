2e9bc7db8bf407d461a23cf2e00ff24252c0c8b5
Rookie, LIN <1049825685@qq.com>
2026-04-05 22:24:29 +0800

    :arrow_up: 升級Expo 55

--- ios/ARKALL/AppDelegate.swift
+++ ios/ARKALL/AppDelegate.swift
@@ -1,10 +1,10 @@
-import Expo
+internal import Expo
 import FirebaseCore
 import React
 import ReactAppDependencyProvider
 
-@UIApplicationMain
-public class AppDelegate: ExpoAppDelegate {
+@main
+class AppDelegate: ExpoAppDelegate {
   var window: UIWindow?
 
   var reactNativeDelegate: ExpoReactNativeFactoryDelegate?
@@ -20,7 +20,6 @@ public class AppDelegate: ExpoAppDelegate {
 
     reactNativeDelegate = delegate
     reactNativeFactory = factory
-    bindReactNativeFactory(factory)
 
 #if os(iOS) || os(tvOS)
     window = UIWindow(frame: UIScreen.main.bounds)
