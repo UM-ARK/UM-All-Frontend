807b0fc2b2deae808c361e960c267883d033a466
Rookie, LIN <1049825685@qq.com>
2026-04-07 04:35:07 +0800

    ✨ 替換SearchBarSection中的Menu元件為DropdownMenu，優化下拉選單功能和樣式

--- src/pages/TabbarPages/what2Reg/components/SearchBarSection.js
+++ src/pages/TabbarPages/what2Reg/components/SearchBarSection.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Keyboard, Pressable, Text, TextInput, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { MenuView } from '@react-native-menu/menu';
+import * as DropdownMenu from 'zeego/dropdown-menu';
+import TouchableScale from 'react-native-touchable-scale';
 import { scale, verticalScale } from 'react-native-size-matters';
 import { t } from 'i18next';
 import { uiStyle } from '../../../../components/ThemeContext';
@@ -79,53 +80,113 @@ const SearchBarSection = ({
                 ) : null}
             </View>
 
-            <MenuView
-                onPressAction={({ nativeEvent }) => {
-                    if (inputOK) {
-                        onPressAction(nativeEvent.event);
+            <DropdownMenu.Root
+                onOpenChange={(open) => {
+                    if (open) {
+                        trigger();
                     }
                 }}
-                actions={[
-                    {
-                        id: 'wiki',
-                        title: `${t('查', { ns: 'catalog' })} ARK Wiki`,
-                        titleColor: themeColor,
-                    },
-                    {
-                        id: 'what2reg',
-                        title: `${t('查', { ns: 'catalog' })} ${t('選咩課', { ns: 'catalog' })}`,
-                        titleColor: black.third,
-                    },
-                    {
-                        id: 'official',
-                        title: `${t('查', { ns: 'catalog' })} ${t('官方', { ns: 'catalog' })}`,
-                        titleColor: black.third,
-                    },
-                ]}
-                shouldOpenOnLongPress={false}
             >
-                <Pressable
-                    style={{
-                        backgroundColor: inputOK ? themeColor : disabled,
-                        borderRadius: scale(6),
-                        padding: scale(7),
-                        paddingHorizontal: scale(8),
-                        alignItems: 'center',
-                    }}
-                    disabled={!inputOK}
-                    onPress={onPressSearchButton}
-                >
-                    <Text style={{
-                        ...uiStyle.defaultText,
-                        fontSize: scale(12),
-                        color: white,
-                        fontWeight: 'bold',
-                        lineHeight: verticalScale(14),
-                    }}>
-                        {t('搜索')}
-                    </Text>
-                </Pressable>
-            </MenuView>
+                <DropdownMenu.Trigger>
+                    <TouchableScale
+                        style={{
+                            backgroundColor: inputOK ? themeColor : disabled,
+                            borderRadius: scale(6),
+                            padding: scale(7),
+                            paddingHorizontal: scale(8),
+                            alignItems: 'center',
+                        }}
+                        activeOpacity={0.8}
+                        disabled={!inputOK}
+                        onPress={onPressSearchButton}
+                    >
+                        <Text style={{
+                            ...uiStyle.defaultText,
+                            fontSize: scale(12),
+                            color: white,
+                            fontWeight: 'bold',
+                            lineHeight: verticalScale(14),
+                        }}>
+                            {t('搜索')}
+                        </Text>
+                    </TouchableScale>
+                </DropdownMenu.Trigger>
+                {/* Menu 選項列表 */}
+                <DropdownMenu.Content>
+                    <DropdownMenu.Item
+                        key="ark-wiki"
+                        onSelect={() => {
+                            trigger();
+                            if (inputOK) {
+                                onPressAction('wiki');
+                            }
+                        }}
+                    >
+                        <DropdownMenu.ItemIcon
+                            ios={{
+                                name: 'book',
+                                pointSize: scale(18),
+                                hierarchicalColor: {
+                                    dark: themeColor,
+                                    light: themeColor,
+                                },
+                            }}
+                            androidIconName="ic_menu_book"
+                        />
+                        <DropdownMenu.ItemTitle style={{ color: themeColor }}>
+                            {t('寫', { ns: 'catalog' })} Wiki
+                        </DropdownMenu.ItemTitle>
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item
+                        key="what2reg"
+                        onSelect={() => {
+                            trigger();
+                            if (inputOK) {
+                                onPressAction('what2reg');
+                            }
+                        }}
+                    >
+                        <DropdownMenu.ItemIcon
+                            ios={{
+                                name: 'star',
+                                pointSize: scale(18),
+                                hierarchicalColor: {
+                                    dark: black.third,
+                                    light: black.third,
+                                },
+                            }}
+                            androidIconName="ic_menu_star"
+                        />
+                        <DropdownMenu.ItemTitle style={{ color: black.third }}>
+                            {t('查', { ns: 'catalog' })} {t('選咩課', { ns: 'catalog' })}
+                        </DropdownMenu.ItemTitle>
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item
+                        key="official"
+                        onSelect={() => {
+                            trigger();
+                            if (inputOK) {
+                                onPressAction('official');
+                            }
+                        }}
+                    >
+                        <DropdownMenu.ItemIcon
+                            ios={{
+                                name: 'graduationcap',
+                                pointSize: scale(18),
+                                hierarchicalColor: {
+                                    dark: black.third,
+                                    light: black.third,
+                                },
+                            }}
+                            androidIconName="ic_menu_myplaces"
+                        />
+                        <DropdownMenu.ItemTitle style={{ color: black.third }}>
+                            {t('查', { ns: 'catalog' })} {t('官方', { ns: 'catalog' })}
+                        </DropdownMenu.ItemTitle>
+                    </DropdownMenu.Item>
+                </DropdownMenu.Content>
+            </DropdownMenu.Root>
         </View>
     );
 };
